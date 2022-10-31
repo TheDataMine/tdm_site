e2e1bc66f38c9d03542fc9e64e1024af0dce0717
nicholasrosenorn <nrosenor@purdue.edu>
2022-10-31 12:30:27 -0400

    Update forms.py

--- tdm_site/projects/forms.py
+++ tdm_site/projects/forms.py
@@ -11,6 +11,9 @@ def get_choices():
     return list(zip(list(Project.objects.order_by().values_list("year", flat=True).distinct()), [int(v) for v in Project.objects.order_by().values_list("year", flat=True).distinct()]))
 
 class SearchProjectsForm(forms.Form):
+    class Meta:
+        labels = {"reg_status": ("Registration Status")}
+        
     year = forms.ChoiceField(choices=get_choices, required=False, widget=forms.Select(attrs={"class": "form-control"}))
     domain = forms.ModelChoiceField(queryset=Domain.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-control"}))
     keywords = forms.ModelMultipleChoiceField(queryset=Keyword.objects.all(), required=False, widget=forms.SelectMultiple(attrs={"class": "form-control"}))
@@ -18,4 +21,4 @@ class SearchProjectsForm(forms.Form):
     citizenship_status = forms.ModelChoiceField(queryset=CitizenshipStatus.objects.all(), required=False, widget=forms.Select(attrs={"class": "form-control"}))
     lab_times = forms.ModelMultipleChoiceField(queryset=Labtime.objects.all(), required=False, widget=forms.SelectMultiple(attrs={"class": "form-control"}))
     lecture_times = forms.ModelMultipleChoiceField(queryset=Lecturetime.objects.all(), required=False, widget=forms.SelectMultiple(attrs={"class": "form-control"}))
-    reg_status = forms.BooleanField(required=False, widget=forms.RadioSelect())
+    reg_status = forms.BooleanField(required=False, widget=forms.RadioSelect(choices=(True, False)))
